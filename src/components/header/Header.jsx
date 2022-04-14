65c4f9bba71ea0465bddbe5572ee1e482f1df3bb
Pohzey <99220043+Pohzey@users.noreply.github.com>
2022-04-13 22:49:22 -0700

    Header, Nav, completed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../img/me.jpg'
+import ME from '../../img/me.png'
 import HeaderSocial from './HeaderSocials'
 
 const header = () => {
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBrain} from 'react-icons/bi'
+import {GoBook} from 'react-icons/go'
+import {BiMessageSquareDetail} from 'react-icons/bi'
 
 const nav = () => {
   return (
-    <div>nav</div>
+    <nav>
+      <a href="#"><AiOutlineHome/></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><BiBrain/></a>
+      <a href="#portfolio"><GoBook/></a>
+      <a href="#contact"><BiMessageSquareDetail/></a>
+    </nav>
   )
 }
 
