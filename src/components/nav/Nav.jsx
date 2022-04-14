917163603dc750fca7757e1dad94f11b7fc1417e
Pohzey <99220043+Pohzey@users.noreply.github.com>
2022-04-14 03:59:29 -0700

    css tweaks color changes

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -15,7 +15,7 @@ const Nav = () => {
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBrain/></a>
       <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><GoBook/></a>
-      <a href="#contact" onClick={() => setActiveNav('#about')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
     </nav>
   )
 }
