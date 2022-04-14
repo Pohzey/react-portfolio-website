134efdf44ee9a2f763552711668c06b8a7617c32
Pohzey <99220043+Pohzey@users.noreply.github.com>
2022-04-14 03:36:02 -0700

    Portfolio component completed

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,62 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../img/portfolio1.jpg'
+import IMG2 from '../../img/portfolio2.png'
+import IMG3 from '../../img/portfolio3.png'
+import IMG5 from '../../img/portfolio5.jpg'
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">Portfolio</section>
+    <section id="portfolio">
+      <h5>View My Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG2} alt="" />
+          </div>
+          <h3>KING OF SPACE: RECONSTRUCTION OF A NARRATIVE GAME</h3>
+          <div className="portfolio__item-cta">
+          <a href="https://github.com/CullenThomas/KingofSpace" className='btn'>GitHub</a>
+          <a href="https://github.com" className='btn btn-primary'>Live Demo</a>
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG3} alt="" />
+          </div>
+          <h3>REACT PORTFOLIO WEBSITE</h3>
+          <div className="portfolio__item-cta">
+          <a href="https://github.com/Pohzey/react-portfolio-website" className='btn'>GitHub</a>
+          <a href="https://github.com" className='btn btn-primary'>Live Demo</a>
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>A JOURNEY THROUGH THE SOLAR SYSTEM</h3>
+          <div className="portfolio__item-cta">
+          <a href="https://github.com" className='btn'>GitHub</a>
+          <a href="https://dtc-wsuv.org/acox21/narrative/" className='btn btn-primary'>Live Demo</a>
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG5} alt="" />
+          </div>
+          <h3>POINT LOCATOR USING MAPBOX API</h3>
+          <div className="portfolio__item-cta">
+          <a href="https://github.com" className='btn'>GitHub</a>
+          <a href="https://dtc-wsuv.org/acox21/mapproject/" className='btn btn-primary'>Live Demo</a>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
